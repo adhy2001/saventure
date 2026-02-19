ab28d23bf2aab5cd0d88cbaefd17dd1067c2aa94
adhy2001 <pe5287423@gmail.com>
2026-02-19 16:35:52 +0530

    Update code

--- blog-data.js
+++ blog-data.js
@@ -3,7 +3,7 @@ const LOUD_BLOG_DATA = [
         "id": "chatgpt-storm",
         "title": "ChatGPT, a new artificial intelligence chatbot, has taken the internet by storm",
         "excerpt": "ChatGPT, an advanced artificial intelligence chatbot, has indeed gained significant attention and popularity across the internet.",
-        "image": "images/blog_1 (1).jpg",
+        "image": "images/blog_1 (1).webp",
         "category": "Strategy",
         "link": "chatgpt-blog.html",
         "delay": 100
@@ -12,7 +12,7 @@ const LOUD_BLOG_DATA = [
         "id": "startup-failuress",
         "title": "Exploring common reasons why many startups often face challenges",
         "excerpt": "Starting a business is an exciting journey, but it's important to be aware of the hurdles that many entrepreneurs face.",
-        "image": "images/blog_2 (1).jpg",
+        "image": "images/blog_2 (1).webp",
         "category": "Entrepreneurship",
         "link": "startup-failure-blog.html",
         "delay": 150
@@ -21,7 +21,7 @@ const LOUD_BLOG_DATA = [
         "id": "business-perspectives",
         "title": "Fresh perspectives on your business or to discuss a new idea from you",
         "excerpt": "In today's fast-paced world, gaining new perspectives and discussing innovative ideas is crucial for staying ahead in business.",
-        "image": "images/blog_3 (1).jpg",
+        "image": "images/blog_3 (1).webp",
         "category": "Innovation",
         "link": "new-age-insights-blog.html",
         "delay": 200
